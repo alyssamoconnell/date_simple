aab6201c3684d6e711033a18d5715f3547a93a04
alyssamoconnell <36858538+alyssamoconnell@users.noreply.github.com>
2018-03-04 15:18:20 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup( name='date_simple',
+       version='0.1',
+       description='This module takes a str and returns a date or takes a date and returns a str. ',
+       url='https://github.com/alyssamoconnell/date_simple',
+       author="Alyssa O'Connell",
+       author_email='ao55@nyu.edu',
+       license='MIT',
+       packages=['date_simple'],
+       install_requires=[ ],
+       zip_safe=False )
